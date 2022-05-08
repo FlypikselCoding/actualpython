619ad53c75b8776cbc10c6687ffce4a8a93d63d7
FlypikselCoding <74743618+FlypikselCoding@users.noreply.github.com>
2022-05-08 15:27:08 -0400

    Add files via upload

---
+++ roadclosed.txt.py
@@ -0,0 +1,16 @@
+print("This road is closed...")
+firstoption = input("""
+Where would you like to go?
+1. North
+2. South
+3. East
+4. West
+Choose wisely...
+""")
+if firstoption == 'North' or 'north':
+    print("""
+You decided to head north.
+A weird sense of dread looms over you.
+You don't know what it is, but you sense
+danger. It's a monster!""")
+    
